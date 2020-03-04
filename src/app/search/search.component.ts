117b26807732db59ee5dee6d11c7e589f312a5d9
grahulpaul <33506388+grahulpaul@users.noreply.github.com>
2020-03-04 20:18:25 +0530

    test

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
@@ -7,13 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class SearchComponent implements OnInit {
-
+  userName = '';
 public vehicle = 'Merce AMG';
-
-
-  constructor() { }
+public vehicleObj :any
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+  }
+  onSubmit(form: NgForm) {
+    if (form.valid) {
+      console.log(form.value.vimNumber);
+   this.ApiCall(form.value.vimNumber);
+    }
+  }
+
+  ApiCall( params : any) {
+   const headers = {
+    "content-type":"application/json",
+    "authorization":"YjNlYjg1MTgtMWJiNS00N2ExLWExZmEtMjljNDljOTBhNjcy",
+    "partner-token":"e2c0534287684c4dbdc3070ee7bedfc6",
+  }  ;
+   this.http.get<any>(`http://api.carmd.com/v3.0/image?vin=${params}`,{headers}).subscribe(data =>{
+     console.log(data);
+     this.vehicleObj = data;
+   });
   }
 
 }
